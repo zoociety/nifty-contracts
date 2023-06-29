f74aad9ba170b35407ab94a6bcca6023be2e03d4
raldblox <raldblox@outlook.com>
2023-06-30 00:35:47 +0800

    Updated hello package

--- contracts/HelloZoociety.sol
+++ contracts/HelloZoociety.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-contract HelloZoociety {
-    constructor() {}
-
+library HelloZoociety {
     function hello() public pure returns (string memory) {
         return "Hello, Zoociety!";
     }
