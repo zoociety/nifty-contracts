ab989c7571c11389b9b12211e3024fcbb9bdfcbf
raldblox <raldblox@outlook.com>
2023-06-29 23:02:33 +0800

    Added HelloZoociety

---
+++ contracts/HelloZoociety.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract HelloZoociety {
+    constructor() {}
+
+    function hello() public pure returns (string memory) {
+        return "Hello, Zoociety!";
+    }
+}
---
+++ contracts/index.js
@@ -0,0 +1,5 @@
+const helloContract = require("../artifacts/contracts/HelloZoociety.sol/HelloZoociety.json");
+
+module.exports = {
+  helloContract,
+};
