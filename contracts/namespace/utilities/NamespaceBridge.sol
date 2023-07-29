5ea26dc254a5352c5b50b81ec2206780a8984674
raldblox <raldblox@outlook.com>
2023-07-29 19:32:15 +0800

    Added: Namespace utilities

---
+++ contracts/namespace/utilities/NamespaceBridge.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract NamespaceConnect {
+    constructor() {}
+
+    // 1. Check if name is owned
+    // 2. Check if space exist
+    // 2. Check if space has factory
+    // 3. Connect and
+}
