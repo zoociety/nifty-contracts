810963c915a33152a65d219468f8259b54db614e
raldblox <raldblox@outlook.com>
2023-08-18 09:29:39 +0800

    updated pragma version

--- contracts/onchainvision/libraries/OCVG.sol
+++ contracts/onchainvision/libraries/OCVG.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.7;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
